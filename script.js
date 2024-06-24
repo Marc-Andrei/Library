ac905e094a387c742b755d518b8f200a0ad8dc7b
Marc-Andrei <marc_andrei_sorin@yahoo.com>
2024-06-24 15:01:25 +0300

    added remove buttons

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addBookToLibrary() {
 
 function addBook(book) {
     const newTr = document.createElement('tr');
+    newTr.class = "Tr";
     for (property in book) {
         const newTd = document.createElement('td');
         newTd.textContent = book[property];
@@ -32,6 +33,37 @@ function clearTable() {
     }
 }
 
+function createRemoveButtons() {
+    /*const rem = document.createElement("button");
+    rem.class = "delete";
+    rem.textContent = "Delete";
+    rem.id = body.children.length - 1;
+    console.log(rem.id);
+    rem.appendChild
+    rem.addEventListener ("click" () => {
+*/
+    while (delButtons.children.length>0) {
+        delButtons.removeChild(delButtons.children[0]);
+}
+    for (let i=0; i < myLibrary.length; i++) {
+        const rem = document.createElement("button");
+        rem.className = "delete";
+        rem.textContent = "Delete";
+        rem.id = `${i}`;
+        rem.addEventListener("click", (event) => {
+            myLibrary.splice(event.target.id, 1);
+            clearTable();
+            updateTable(myLibrary);
+            createRemoveButtons();
+        } )
+
+        delButtons.appendChild(rem);
+
+
+        
+    }
+    }
+
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
 const deepWork = new Book("Deep Work", "Cal Newport", "230", "reading");
 const ess = new Book("ess", "Cal Newport", "230", "reading");
@@ -39,9 +71,9 @@ const ess = new Book("ess", "Cal Newport", "230", "reading");
 const library = [hobbit, deepWork, ess];
 const myLibrary = [];
 
+const tableDiv = document.querySelector(".table") 
+const delButtons = document.querySelector(".delButtons")
 const body = document.querySelector('tbody');
-const th = document.createElement('th');
-const tr = document.createElement('tr')
 const newBookBtn = document.querySelector('.new');
 const dialog = document.querySelector("dialog");
 const submit = document.querySelector(".submitBook");
@@ -57,11 +89,14 @@ newBookBtn.addEventListener ("click", () => {
 submit.addEventListener ("click", (event) => {
     event.preventDefault();
     const newBook = new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value);
+    newBookTitle.value = "";
+    newBookAuthor.value = "";
+    newBookPages.value = "";
+    newBookRead.value = "";
     myLibrary.push(newBook);
     clearTable();
     updateTable(myLibrary);
-
-
+    createRemoveButtons();
     dialog.close();
 });
 
